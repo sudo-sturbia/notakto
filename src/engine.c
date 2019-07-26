8c44c88addc2b24073878261f8e1a6b3892e97a9
sudo-sturbia <49781778+sudo-sturbia@users.noreply.github.com>
2019-07-26 10:43:13 +0200

    Board/configuration comparison.

--- src/engine.c
+++ src/engine.c
@@ -10,7 +10,8 @@ typedef struct boardValue
 }boardValue;
 
 /* DEFINITIONS */
-#define NO_BOARDS 3
+#define NO_BOARDS    3
+#define NO_ROTATIONS 8
 
 // Board values
 #define A 2
@@ -30,8 +31,9 @@ typedef struct boardValue
 extern int boards[NO_BOARDS][3][3];
 extern int dead_boards[NO_BOARDS];
 
-// All boards configurations (except dead boards)
-// Boards with the same no. of Xs are grouped
+/* BOARDS CONFIGURATIONS */
+// All possible configs (except dead boards)
+// Boards with the same no. of Xs are grouped into arrays
 const boardValue X0[ZERO_X] = {   {{{0, 0, 0},                     // Configuration 1
                                     {0, 0, 0},
                                     {0, 0, 0}}, {C, 0}}   };
@@ -215,3 +217,71 @@ const boardValue X5[FIVE_X] = {   {{{1, 1, 0},                     // Configurat
 const boardValue X6[SIX_X] = {    {{{1, 1, 0},                     // Configuration 1
                                     {1, 0, 1},
                                     {0, 1, 1}}, {A, 0}}   };
+
+/* FUNCTIONS */
+int compare();
+int compare_boards(int board1[3][3], int board2[3][3]);
+
+void rotate_board(int board[3][3], int rotations[NO_ROTATIONS][3][3]);
+
+// Compares board with a configuration
+// Returns 1: if the same, 0: otherwise
+int compare(int board[3][3], boardValue config)
+{
+    // Find all rotations of a configuration
+    int rotations[NO_ROTATIONS][3][3];
+    rotate_board(config.board, rotations);
+
+    // Compare board with rotations
+    for (int i = 0; i < NO_ROTATIONS; i++)
+    {
+        if (compare_boards(board, rotations[i]))
+        {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+// Compare two boards
+int compare_boards(int board1[3][3], int board2[3][3])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            // If there's a difference
+            if (board1[i][j] != board2[i][j])
+            {
+                return 0;
+            }
+        }
+    }
+
+    // There's no difference
+    return 1;
+}
+
+// Produce all (8) possible rotations of a board configuration
+// Fills array with 8 boards
+void rotate_board(int board[3][3], int rotations[NO_ROTATIONS][3][3])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            rotations[0][i][j] = board[i][j];
+            rotations[1][i][j] = board[i][2 - j];
+
+            rotations[2][i][j] = board[j][i];
+            rotations[3][i][j] = board[j][2 - i];
+
+            rotations[4][i][j] = board[2 - i][j];
+            rotations[5][i][j] = board[2 - i][2 - j];
+
+            rotations[6][i][j] = board[2 - j][i];
+            rotations[7][i][j] = board[2 - j][2 - i];
+        }
+    }
+}
--- src/engine.h
+++ src/engine.h
@@ -2,8 +2,15 @@
 #define ENGINE_H_INCLUDED
 
 /* DEFINITIONS */
+#define NO_BOARDS    3
+#define NO_ROTATIONS 8
+
 typedef struct boardValue boardValue;
 
 /* FUNCTIONS */
+int compare();
+int compare_boards(int board1[3][3], int board2[3][3]);
+
+void rotate_board(int board[3][3], int rotations[NO_ROTATIONS][3][3]);
 
 #endif 
